1b54c0478410e12bf8d94862ef3cdb65713e9e52
Musah Morad <musahmorad01@gmail.com>
2024-07-17 16:07:47 -0400

    feat: Add composer install route to web.php and ComposerController

---
+++ app/Http/Controllers/ComposerController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+
+class ComposerController extends Controller
+{
+    public function install()
+    {
+        $process = new Process(['composer', 'install']);
+        $process->run();
+
+        // Executes after the command finishes
+        if (!$process->isSuccessful()) {
+            throw new ProcessFailedException($process);
+        }
+
+        return response()->json([
+            'output' => $process->getOutput(),
+            'error' => $process->getErrorOutput(),
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ComposerController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// routes/web.php
+Route::get('/composer-install', [ComposerController::class, 'install'])->middleware('auth');
+
