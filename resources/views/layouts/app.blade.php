8e052934c0f436912775adabd6b490b465837c39
Musah Morad <musahmorad01@gmail.com>
2024-10-09 09:03:53 -0400

    "Updated jQuery version from 3.5.1.min.js to 3.5.1.slim.min.js"

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -210,7 +210,7 @@
     <!-- Bootstrap JS and dependencies -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js" defer></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.0/lazysizes.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     {{-- <script src="{{ mix('js/app.js') }}"></script> --}}
